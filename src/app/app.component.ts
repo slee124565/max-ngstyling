36ac2de4310fc8a7aca309481382502544f7768c
Lee Shiueh <lee.shiueh@gmail.com>
2019-08-25 21:54:27 +0800

    first import and setup prj-styling-start base source code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courseGoals = [
-    { title: 'Master Angular Styling', isActiveGoal: true },
-    { title: 'Understand Angular Animations', isActiveGoal: false },
-    { title: 'Master Angular Animations', isActiveGoal: false },
-  ];
-
 }

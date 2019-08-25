3cfa024558449753f84f5bf97776b0e0b6141473
Lee Shiueh <lee.shiueh@gmail.com>
2019-08-25 13:31:38 +0800

    第 1 節：Getting Started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngstyle-max';
+  courseGoals = [
+    { title: 'Master Angular Styling', isActiveGoal: true },
+    { title: 'Understand Angular Animations', isActiveGoal: false },
+    { title: 'Master Angular Animations', isActiveGoal: false },
+  ];
+
 }
